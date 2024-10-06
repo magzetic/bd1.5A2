516b0e6dfdbbd5b9a7187f68ea253aaa4036991f
magzetic <magzetic@gmail.com>
2024-10-06 17:19:03 +0530

    updated

--- index.js
+++ index.js
@@ -5,47 +5,49 @@ let cors = require('cors');
 const app = express();
 app.use(cors());
 const port = 3000;
-
 app.get('/calculate-returns', (req, res) => {
   const boughtAt = parseFloat(req.query.boughtAt);
   const marketPrice = parseFloat(req.query.marketPrice);
   const quantity = parseFloat(req.query.quantity);
+
   const returns = (marketPrice - boughtAt) * quantity;
   res.send(returns.toString());
 });
-
 app.get('/total-returns', (req, res) => {
   const stock1 = parseFloat(req.query.stock1);
   const stock2 = parseFloat(req.query.stock2);
   const stock3 = parseFloat(req.query.stock3);
   const stock4 = parseFloat(req.query.stock4);
+
   const totalReturns = stock1 + stock2 + stock3 + stock4;
   res.send(totalReturns.toString());
 });
-
 app.get('/calculate-return-percentage', (req, res) => {
   const boughtAt = parseFloat(req.query.boughtAt);
   const returns = parseFloat(req.query.returns);
+
   const returnPercentage = (returns / boughtAt) * 100;
   res.send(returnPercentage.toString());
 });
+app.get('/total-return-percentage', (req, res) => {
+  const stock1 = parseFloat(req.query.stock1);
+  const stock2 = parseFloat(req.query.stock2);
+  const stock3 = parseFloat(req.query.stock3);
+  const stock4 = parseFloat(req.query.stock4);
 
-app.get('/calculate-return-percentage', (req, res) => {
-  const boughtAt = parseFloat(req.query.boughtAt);
-  const returns = parseFloat(req.query.returns);
-  const returnPercentage = (returns / boughtAt) * 100;
-  res.send(returnPercentage.toString());
+  const totalReturnPercentage = stock1 + stock2 + stock3 + stock4;
+  res.send(totalReturnPercentage.toString());
 });
-
 app.get('/status', (req, res) => {
   const returnPercentage = parseFloat(req.query.returnPercentage);
 
   if (returnPercentage > 0) {
-    res.send('profit');
+      res.send('profit');
   } else {
-    res.send('loss');
+      res.send('loss');
   }
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
